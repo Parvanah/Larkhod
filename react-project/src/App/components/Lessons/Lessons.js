b499d8d251c56b80c0c23fc3cc20372e432f2903
ShamsiaSadat <sadatshamsia2@gmail.com>
2023-05-24 11:31:56 +0430

    updated

--- react-project/src/App/components/Lessons/Lessons.js
+++ react-project/src/App/components/Lessons/Lessons.js
@@ -14,29 +14,27 @@ const Lessons = (props) => {
   const lessons = locationData.lessons;
   const unit = locationData.title;
   const [title, setTitle] = useState("");
+  
   return (
-    <div className="lesson">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width={500}
-        height={620}
-        {...props}
+    <div>
+      <div className="lesson">
+
+        <svg xmlns="http://www.w3.org/2000/svg" width={403} height={675} {...props}>
+    <defs>
+      <linearGradient
+        id="a"
+        x1={0.802}
+        x2={0.459}
+        y1={0.246}
+        y2={1.696}
+        gradientUnits="objectBoundingBox"
       >
-        <defs>
-          <linearGradient
-            id="a"
-            x1={0.802}
-            x2={0.459}
-            y1={0.246}
-            y2={1.696}
-            gradientUnits="objectBoundingBox"
-          >
-            <stop offset={0} stopColor="#3c98bd" />
-            <stop offset={1} stopColor="#0f53a1" />
-          </linearGradient>
-        </defs>
-        <path fill="url(#a)" d="M0 0h403v1080H0z" data-name="Rectangle 237" />
-        <foreignObject
+        <stop offset={0} stopColor="#3c98bd" />
+        <stop offset={1} stopColor="#0f53a1" />
+      </linearGradient>
+    </defs>
+    <path fill="url(#a)" d="M0 0h403v1080H0z" data-name="Rectangle 237" />
+    <foreignObject
           width={"100%"}
           height={"100%"}
           x={6}
@@ -70,7 +68,7 @@ const Lessons = (props) => {
             </div>
           </div>
         </foreignObject>
-      </svg>
+        </svg>
       <div className="showLesson">
         <div className="title">
           <div>
@@ -78,17 +76,19 @@ const Lessons = (props) => {
             <p>درس {title}</p>
           </div>
           <img src={photo1}></img>
+          
         </div>
         <div className="bottom">
           <div className="img">
-            {" "}
             <img src={photo2}></img>
           </div>
         </div>
         <div className="midlle">
           <img src={photo3}></img>
           <p>دانلود فایل پی دی اف</p>
+          
         </div>
+      </div>
       </div>
     </div>
   );
