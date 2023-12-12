131c5b9d25f3514964f0f57fff671b88499a0873
Khatima Sadat <khatimasadat70@gmail.com>
2023-12-12 15:16:02 +0430

    icon and title on website tab updated

--- WebSite/src/App/components/Header/Header.js
+++ WebSite/src/App/components/Header/Header.js
@@ -17,6 +17,7 @@ import arrowOpen from "../../assets/Path-1005.png";
 import arrowClose from "../../assets/Path-1006.png";
 import { ImCross } from "react-icons/im";
 import i18n from "../../../i18n";
+import Home from "../Home/Home";
 // import butimg from "../../assets/Path_7";
 // import buttom_img from "../../assets/Path_7.png";
 // import down_img from "../../assets/Path_39.png";
@@ -99,12 +100,12 @@ const Header = ({ t }, props) => {
                 {t("header5")}
               </Link>
             </li>
-            <hr className="vl_header" />
+            {/* <hr className="vl_header" />
             <li className="li_header">
               <Link className="link_nav_header" to={nav} state={status}>
                 {t("header6")}
               </Link>
-            </li>
+            </li> */}
             <hr className="vl_header" />
             <li className="li_header">
               <Link className="link_nav_header" to="/about">
@@ -172,46 +173,7 @@ const Header = ({ t }, props) => {
           )}
         </div>
       </nav>
-      <div className="home">
-        <Link to="/section">
-          <svg class="home_svg" viewBox="385.11 697.15 30.158 16.435">
-            <path
-              id="home_svg"
-              d="M 415.2678833007812 647.1500854492188 L 400.18896484375 663.585205078125 L 385.1099853515625 647.1500854492188 L 393.4033813476562 647.1500854492188 L 400.18896484375 654.5338745117188 L 406.9443359375 647.1500854492188 L 415.2678833007812 647.1500854492188 Z"
-            ></path>
-          </svg>
-        </Link>
-        <div className="images_paragraph_home">
-          <div>
-            <img className="imgg_home" src={imgg} />
-            <img className="img_one_home" src={img1} />
-            <img className="imggg_home" src={img} />
-          </div>
-          <div>
-            <samp className="paragraph_home">{t("header1")}</samp>
-            <samp className="paragraph_1_home">
-              <br /> {t("header2")}
-            </samp>
-          </div>
-        </div>
-        <svg className="home_svg_one" viewBox="131.89 248.487 1920.661 112.903">
-          <linearGradient
-            id="Union_1_dg"
-            spreadMethod="pad"
-            x1="0.5"
-            x2="0.912"
-            y1="0"
-            y2="1.944"
-          >
-            <stop offset="0" stop-color="#3c98bd" stop-opacity="1"></stop>
-            <stop offset="1" stop-color="#0f53a1" stop-opacity="1"></stop>
-          </linearGradient>
-          <path
-            id="home_svg_one"
-            d="M 1052.548828125 411.3898620605469 L 131.8900146484375 411.3898620605469 L 131.8900146484375 286.3383178710938 C 770.7160034179688 235.870361328125 1413.725708007812 235.870361328125 2052.550537109375 286.338134765625 L 2052.548828125 911.3898620605469 Z"
-          ></path>
-        </svg>
-      </div>
+      <Home />
       <div className="mobile">
         <div className="top">
           <img src={topimag} />
@@ -245,6 +207,28 @@ const Header = ({ t }, props) => {
               </ul>
             </div>
           )}
+          {/* <div className="lng">
+            <div className="selctedoption">
+              <div>{selectedOption || t("header11")}</div>
+              <div onClick={handleToggle} style={{ display: isClose }}>
+                <img src={arrowOpen} />
+              </div>
+              <div style={{ display: isOpen }} onClick={handleToggle}>
+                <img src={arrowClose} />
+              </div>
+            </div>
+            <div className="lng-menu">
+              {isOpen === "flex" && (
+                <ul className="dropdown-menu">
+                  {options.map((option) => (
+                    <li key={option} onClick={() => handleOptionClick(option)}>
+                      {option.name}
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </div>
+          </div> */}
           <div className="menu" onClick={handleSideBar}>
             <FiMenu />
           </div>
@@ -288,12 +272,12 @@ const Header = ({ t }, props) => {
                 {t("header5")}
               </Link>
             </li>
-            <hr />
+            {/* <hr />
             <li className="li_header">
               <Link className="link_nav_header" to="/continue">
                 {t("header6")}
               </Link>
-            </li>
+            </li> */}
             <hr />
             <li className="li_header">
               <Link className="link_nav_header" to="/about">
