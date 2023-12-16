4819a24483b438f29f92641bebfea90fe4b7c743
nooria ehsani <ehsaninoor224@gmail.com>
2023-12-16 23:11:37 +0430

    reading section edited

--- WebSite/src/readingBook.js
+++ WebSite/src/readingBook.js
@@ -1,10 +1,28 @@
 import * as React from "react";
 import { withNamespaces } from "react-i18next";
+import { Link, useLocation } from "react-router-dom";
 import "./readingBook.css";
+import reading from "./App/assets/reading.png";
+import backr from "./App/assets/backr.png";
 const ReadingBook = ({ t }) => {
   return (
     <div className="readingBookk">
-      ‌<div className="topOfbook">Books For reading</div>‌
+    <div className="topOfbook">
+      <div className="backslashR">
+      <Link to="/header">
+      <img src={backr} className="backslashR"/>
+          </Link>
+       
+      </div>
+     <div className="bottomOfBook">
+     ‌<div className="r_text">
+        <p>{t( "rtext")}</p>
+        </div>
+      <div className="readingBooks">
+      <img src={reading}  />
+        </div>
+     </div>
+        </div>‌
       {/* <div className="bottomOfBook">gggg</div> */}
     </div>
   );
