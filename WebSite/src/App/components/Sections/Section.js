28a4a2a38557d96104a3f929bf8ec02a2b7c8526
Parvanah <parvanahomarzai@gmail.com>
2023-12-25 10:16:32 -0800

    update Section Page

--- WebSite/src/App/components/Sections/Section.js
+++ WebSite/src/App/components/Sections/Section.js
@@ -36,7 +36,7 @@ const Section = ({ t }, props) => {
     <div className="full-section">
       <div className="section">
         <div className="section_one_images">
-        <div className="prevuse_section">
+          <div className="prevuse_section">
             <img className="prevuse" onClick={() => navigate("/home")} src={image4} />
             <img className="svgone_section" src={svgone} />
           </div>
@@ -51,7 +51,7 @@ const Section = ({ t }, props) => {
         </div>
         {/* ul-section */}
         <ul className="section_three_ul">
-        <li>
+          <li>
             <Link
               to="/grades"
               state={{
@@ -122,109 +122,6 @@ const Section = ({ t }, props) => {
           <img className="svg-mobile-section-two" src={Shape3} />
         </div>
       </div>
-
-
-
-
-
-
-
-
-
-      {/* <div className="mobile-section">
-        <div className="mob-sec-header">
-          <div className="prevuse" onClick={() => navigate(-1)}>
-            <img src={image4} />
-          </div>
-          <div id="title">
-            <h1>{t("section1")}</h1>
-            <p>{t("section2")}</p>
-          </div>
-          <div className="shape-1">
-            <img src={shape1} />
-          </div>
-          <div className="shape-2">
-            <img src={shape2} />
-          </div>
-        </div>
-        <div className="mob-sec-article">
-          <ul id="ul-section">
-            <li>
-              <Link
-                to="/grades"
-                state={{
-                  Dari: {
-                    grades: dataDari.primary.grades,
-                    school_path: base_path + dataDari.primary.base_path,
-                  },
-                  Pashto: {
-                    grades: dataPashto.primary.grades,
-                    school_path: base_path + dataPashto.primary.base_path,
-                  },
-                }}
-              >
-                <img src={image1} />
-                <h1>{t("section3")}</h1>
-                <p>{t("section4")}</p>
-              <p>{t("section13")}</p>
-              <p>{t("section14")}</p>
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/grades"
-                state={{
-                  Dari: {
-                    grades: dataDari.secondary.grades,
-                    school_path: base_path + dataDari.secondary.base_path,
-                  },
-                  Pashto: {
-                    grades: dataPashto.secondary.grades,
-                    school_path: base_path + dataPashto.secondary.base_path,
-                  },
-                }}
-              >
-                <img src={image2} />
-                <h1>{t("section5")} </h1>
-                <p>{t("section6")}</p>
-              <p>{t("section11")}</p>
-              <p>{t("section12")}</p>
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/grades"
-                state={{
-                  Dari: {
-                    grades: dataDari.high_school.grades,
-                    school_path: base_path + dataDari.high_school.base_path,
-                  },
-                  Pashto: {
-                    grades: dataPashto.high_school.grades,
-                    school_path: base_path + dataPashto.high_school.base_path,
-                  },
-                }}
-              >
-                <img src={image3} />
-                <h1>{t("section7")} </h1>
-                <p>{t("section8")}</p>
-              <p>{t("section9")}</p>
-              <p>{t("section10")}</p>
-                <p></p>
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <div className="mob-sec-bottom">
-          <div className="shape-3">
-            <img src={shape2} />
-          </div>
-          <div className="shape-4">
-            <img src={Shape3} />
-          </div>
-        </div>
-        <div />
-      </div> */}
     </div>
   );
   //  }
