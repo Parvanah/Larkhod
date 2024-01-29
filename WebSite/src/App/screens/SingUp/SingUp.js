6d5a3ff560263be1caff837ac9fcaa0bb96cfdb4
Parvanah <parvanahomarzai@gmail.com>
2024-01-29 10:17:56 -0800

    change

--- WebSite/src/App/screens/SingUp/SingUp.js
+++ WebSite/src/App/screens/SingUp/SingUp.js
@@ -13,7 +13,7 @@ import img3 from "../../assets/logo_1.png";
 import React from "react";
 import { Link, useNavigate, useNavigation } from "react-router-dom";
 import LogIn from "../Login/LogIn";
-import { number } from "yup";
+// import { number } from "yup";
 <link rel="stylesheet" href="SingUp.css" />;
 // import React, { useState } from 'react';
   
